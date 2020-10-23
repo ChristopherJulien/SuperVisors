7afa8ef24a842506abe4949a6e31654e2840679e
Nathanaël <73140662+NBourgeois7@users.noreply.github.com>
2020-10-23 11:05:13 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,6 @@
+bool speed  //0 if below speed limit, otherwise 1
+bool rain  //0 if below no rain, otherwise 1
+bool sun  //0 if below no sun, otherwise 1
+bool measureValues = 0
+bool manualMode = 0
+volatile char State = ‘open’  //use enum
