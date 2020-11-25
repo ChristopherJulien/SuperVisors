6c0b18999909bfdd0c1b18ff4729b33f643e60e0
JulienStocker <julienstocker@icloud.com>
2020-11-25 16:39:23 +0100

    New Files
    new files

--- mainPIO_UNO/src/main.cpp
+++ mainPIO_UNO/src/main.cpp
@@ -1,71 +1,50 @@
 #include <Arduino.h>
 
+const int ph_A    = A3;
+const int ph_B    = A2;
+const int ph_C    = A1;
+const int ph_D    = A0;
 
-const int ph_FRONT_TOP    = A1;
-const int ph_FRONT_BOTTOM = A0;
-const int ph_TOP_RIGHT    = A5;
-const int ph_TOP_LEFT     = A2;
-const int ph_TOP_FORWARD  = A4;
-const int ph_TOP_BACKWARD = A3;
-
-int val_ph_FRONT_TOP    = 0;
-int val_ph_FRONT_BOTTOM = 0;
-int val_ph_TOP_RIGHT    = 0;
-int val_ph_TOP_LEFT     = 0;
-int val_ph_TOP_FORWARD  = 0;
-int val_ph_TOP_BACKWARD =0;
+int val_ph_A    = 0;
+int val_ph_B    = 0;
+int val_ph_C    = 0;
+int val_ph_D    = 0;
 
 
+int iterations = 100;
+int i = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(ph_FRONT_TOP, INPUT);
-  pinMode(ph_FRONT_BOTTOM, INPUT);
-
-  pinMode(ph_TOP_RIGHT, INPUT);
-  pinMode(ph_TOP_LEFT, INPUT);
-  pinMode(ph_TOP_FORWARD, INPUT);
-  pinMode(ph_TOP_BACKWARD, INPUT);
+  pinMode(ph_A, INPUT);
+  pinMode(ph_B, INPUT);
+  pinMode(ph_C, INPUT);
+  pinMode(ph_D, INPUT);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  val_ph_FRONT_TOP = analogRead(ph_FRONT_TOP);
-  val_ph_FRONT_BOTTOM = analogRead(ph_FRONT_BOTTOM);
-  val_ph_TOP_RIGHT = analogRead(ph_TOP_RIGHT);
-  val_ph_TOP_LEFT = analogRead(ph_TOP_LEFT);
-  val_ph_TOP_FORWARD = analogRead(ph_TOP_FORWARD);
-  val_ph_TOP_BACKWARD = analogRead(ph_TOP_BACKWARD);
+  val_ph_A = analogRead(ph_A);
+  val_ph_B = analogRead(ph_B);
+  val_ph_C = analogRead(ph_C);
+  val_ph_D = analogRead(ph_D);
 
 
-String format = "%-20s %5d\n";
-Serial.println("");
-System.out.format(format, "FT", val_ph_FRONT_TOP);
-System.out.format(format, "\t FB", val_ph_FRONT_BOTTOM);
-System.out.format(format, "\t TF", val_ph_TOP_FORWARD);
-System.out.format(format, "\t TB", val_ph_TOP_BACKWARD);
-System.out.format(format, "\t TR", val_ph_TOP_RIGHT);
-System.out.format(format, "\t TL", val_ph_TOP_LEFT);
-Serial.println("");
-
-  // Serial.println("");
-  // Serial.print("FT: ");
-  // Serial.print(val_ph_FRONT_TOP);
-  // Serial.print("   FB: ");
-  // Serial.print(val_ph_FRONT_BOTTOM);
-  // Serial.print("   TF: ");
-  // Serial.print(val_ph_TOP_FORWARD);
-  // Serial.print("   TB: ");
-  // Serial.print(val_ph_TOP_BACKWARD);
-  // Serial.print("   TR: ");
-  // Serial.print(val_ph_TOP_RIGHT);
-  // Serial.print("   TL: ");
-  // Serial.print(val_ph_TOP_LEFT);
-  // Serial.println("");
-
-
+if(i< iterations){
+  Serial.println("");
+  Serial.print("A: ");
+  Serial.print(val_ph_A);
+  Serial.print("   B: ");
+  Serial.print(val_ph_B);
+  Serial.print("   C: ");
+  Serial.print(val_ph_C);
+  Serial.print("   D: ");
+  Serial.print(val_ph_D);
+  Serial.println("");
   delay(500);
-  
+  // i++;
+  }
 
 }
